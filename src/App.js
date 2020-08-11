5cbba488759eea30c854666ed3b8f24486bfd05a
DavydovDenys <denvanrain@gmail.com>
2020-08-11 22:03:29 +0300

    add togglePersonsHandler

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, {Component} from 'react';
 import './App.css';
+import Person from './Person/Person'
+
 
 class App extends Component {
   state = {
     persons: [
       {id: 1, name: 'Max', age: 28},
-      {id: 2, name: 'Armin', age: 28},
-      {id: 3, name: 'Tom', age: 28},
+      {id: 2, name: 'Armin', age: 25},
+      {id: 3, name: 'Tom', age: 22},
     ],
     otherState: 'some other state',
     showPersons: false
@@ -37,13 +39,51 @@ class App extends Component {
     // обновляем массив - заменяем элемент на новый с обновленным именем
     persons[personIndex] = person;
 
+    // обновляем state
     this.setState({persons: persons});
+  };
+
+  togglePersonsHandler = () => {
+    const doesShow = this.state.showPersons;
+    this.setState({showPersons: !doesShow});
   }
 
   render() {
+
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '1px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    };
+
+    let persons = null;
+
+    if (this.state.showPersons) {
+      persons = (
+        <div>
+          {this.state.persons.map((person, index) => {
+            return (
+              <Person
+              name={person.name}
+              age={person.age}
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id) }/>
+            )
+          })}
+        </div>
+      );
+    }
+
   return (
     <div className="App">
-
+      <h1>Hi, I'm a React App</h1>
+      <p>This is really working!</p>
+      <button
+        style={style}
+        onClick={this.togglePersonsHandler}>Toggle Persons</button>
+      {persons}
     </div>
   );
   }
--- src/Person/Person.js
+++ src/Person/Person.js
@@ -1,11 +1,11 @@
 import React from "react";
-import 'Person.css/'
+import './Person.css'
 
 
 const person = (props) => {
   return (
     <div className='Person'>
-      <p></p>
+      <p>I'm {props.name} and I am {props.age} years old!</p>
       <p></p>
       <input onChange={props} type="text"/>
     </div>
